c42a300aa6dc99299202901d33e80fac280ad067
Alexandru Mihai <a_mihai_lo@yahoo.ro>
2020-03-25 15:51:56 +0200

    fix2

---
+++ build/generated/ap_generated_sources/debug/out/com/example/myapplication/Database_Impl.java
@@ -0,0 +1,133 @@
+package com.example.myapplication;
+
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomOpenHelper;
+import androidx.room.RoomOpenHelper.Delegate;
+import androidx.room.util.DBUtil;
+import androidx.room.util.TableInfo;
+import androidx.room.util.TableInfo.Column;
+import androidx.room.util.TableInfo.ForeignKey;
+import androidx.room.util.TableInfo.Index;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Callback;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Configuration;
+import java.lang.IllegalStateException;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class Database_Impl extends Database {
+  private volatile StudentDAO _studentDAO;
+
+  @Override
+  protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration configuration) {
+    final SupportSQLiteOpenHelper.Callback _openCallback = new RoomOpenHelper(configuration, new RoomOpenHelper.Delegate(1) {
+      @Override
+      public void createAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("CREATE TABLE IF NOT EXISTS `Student` (`name` TEXT NOT NULL, `mark` REAL NOT NULL, PRIMARY KEY(`name`))");
+        _db.execSQL("CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)");
+        _db.execSQL("INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, \"deca583e6b43f2d6abb2720678f87bba\")");
+      }
+
+      @Override
+      public void dropAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("DROP TABLE IF EXISTS `Student`");
+      }
+
+      @Override
+      protected void onCreate(SupportSQLiteDatabase _db) {
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onCreate(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onOpen(SupportSQLiteDatabase _db) {
+        mDatabase = _db;
+        internalInitInvalidationTracker(_db);
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onOpen(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onPreMigrate(SupportSQLiteDatabase _db) {
+        DBUtil.dropFtsSyncTriggers(_db);
+      }
+
+      @Override
+      public void onPostMigrate(SupportSQLiteDatabase _db) {
+      }
+
+      @Override
+      protected void validateMigration(SupportSQLiteDatabase _db) {
+        final HashMap<String, TableInfo.Column> _columnsStudent = new HashMap<String, TableInfo.Column>(2);
+        _columnsStudent.put("name", new TableInfo.Column("name", "TEXT", true, 1));
+        _columnsStudent.put("mark", new TableInfo.Column("mark", "REAL", true, 0));
+        final HashSet<TableInfo.ForeignKey> _foreignKeysStudent = new HashSet<TableInfo.ForeignKey>(0);
+        final HashSet<TableInfo.Index> _indicesStudent = new HashSet<TableInfo.Index>(0);
+        final TableInfo _infoStudent = new TableInfo("Student", _columnsStudent, _foreignKeysStudent, _indicesStudent);
+        final TableInfo _existingStudent = TableInfo.read(_db, "Student");
+        if (! _infoStudent.equals(_existingStudent)) {
+          throw new IllegalStateException("Migration didn't properly handle Student(com.example.myapplication.Student).\n"
+                  + " Expected:\n" + _infoStudent + "\n"
+                  + " Found:\n" + _existingStudent);
+        }
+      }
+    }, "deca583e6b43f2d6abb2720678f87bba", "8918f34f7d8342c48781de2b1f462391");
+    final SupportSQLiteOpenHelper.Configuration _sqliteConfig = SupportSQLiteOpenHelper.Configuration.builder(configuration.context)
+        .name(configuration.name)
+        .callback(_openCallback)
+        .build();
+    final SupportSQLiteOpenHelper _helper = configuration.sqliteOpenHelperFactory.create(_sqliteConfig);
+    return _helper;
+  }
+
+  @Override
+  protected InvalidationTracker createInvalidationTracker() {
+    final HashMap<String, String> _shadowTablesMap = new HashMap<String, String>(0);
+    HashMap<String, Set<String>> _viewTables = new HashMap<String, Set<String>>(0);
+    return new InvalidationTracker(this, _shadowTablesMap, _viewTables, "Student");
+  }
+
+  @Override
+  public void clearAllTables() {
+    super.assertNotMainThread();
+    final SupportSQLiteDatabase _db = super.getOpenHelper().getWritableDatabase();
+    try {
+      super.beginTransaction();
+      _db.execSQL("DELETE FROM `Student`");
+      super.setTransactionSuccessful();
+    } finally {
+      super.endTransaction();
+      _db.query("PRAGMA wal_checkpoint(FULL)").close();
+      if (!_db.inTransaction()) {
+        _db.execSQL("VACUUM");
+      }
+    }
+  }
+
+  @Override
+  public StudentDAO studentDAO() {
+    if (_studentDAO != null) {
+      return _studentDAO;
+    } else {
+      synchronized(this) {
+        if(_studentDAO == null) {
+          _studentDAO = new StudentDAO_Impl(this);
+        }
+        return _studentDAO;
+      }
+    }
+  }
+}
---
+++ build/generated/ap_generated_sources/debug/out/com/example/myapplication/StudentDAO_Impl.java
@@ -0,0 +1,119 @@
+package com.example.myapplication;
+
+import android.database.Cursor;
+import androidx.lifecycle.LiveData;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class StudentDAO_Impl implements StudentDAO {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter __insertionAdapterOfStudent;
+
+  private final SharedSQLiteStatement __preparedStmtOfDelete;
+
+  public StudentDAO_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfStudent = new EntityInsertionAdapter<Student>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `Student`(`name`,`mark`) VALUES (?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Student value) {
+        if (value.name == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.name);
+        }
+        stmt.bindDouble(2, value.mark);
+      }
+    };
+    this.__preparedStmtOfDelete = new SharedSQLiteStatement(__db) {
+      @Override
+      public String createQuery() {
+        final String _query = "DELETE FROM student WHERE name = ?";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public void insert(final Student student) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfStudent.insert(student);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public int delete(final String studentName) {
+    __db.assertNotSuspendingTransaction();
+    final SupportSQLiteStatement _stmt = __preparedStmtOfDelete.acquire();
+    int _argIndex = 1;
+    if (studentName == null) {
+      _stmt.bindNull(_argIndex);
+    } else {
+      _stmt.bindString(_argIndex, studentName);
+    }
+    __db.beginTransaction();
+    try {
+      final int _result = _stmt.executeUpdateDelete();
+      __db.setTransactionSuccessful();
+      return _result;
+    } finally {
+      __db.endTransaction();
+      __preparedStmtOfDelete.release(_stmt);
+    }
+  }
+
+  @Override
+  public LiveData<List<Student>> findAll() {
+    final String _sql = "SELECT name, mark FROM student";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return __db.getInvalidationTracker().createLiveData(new String[]{"student"}, false, new Callable<List<Student>>() {
+      @Override
+      public List<Student> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false);
+        try {
+          final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+          final int _cursorIndexOfMark = CursorUtil.getColumnIndexOrThrow(_cursor, "mark");
+          final List<Student> _result = new ArrayList<Student>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final Student _item;
+            _item = new Student();
+            _item.name = _cursor.getString(_cursorIndexOfName);
+            _item.mark = _cursor.getFloat(_cursorIndexOfMark);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+}
